aacb9fd1ae6cda396be453f17b5526bd65a7451a
Taz Uddin <getleton@gmail.com>
2019-05-24 11:25:12 +0600

    Test added

--- BongoCodingTest/TestBongoCodingTestNet/TestJsonParser.cs
+++ BongoCodingTest/TestBongoCodingTestNet/TestJsonParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using BongoCodingTestNet;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,7 +13,23 @@ namespace TestBongoCodingTestNet
         {
             string json = "{\"Key1\": \"1\",\"Key2\": {\"Key3\": \"1\",\"Key4\": {\"Key5\": \"4\"}}}";
             NestedJsonParser parser = new NestedJsonParser();
-            parser.Deserialize(json).Print();            
+            parser.Deserialize(json)
+                  .Print();            
         }
+        [TestMethod]
+        public void ShouldParseNestedJsonStringAndPrintWithDepthWithCsharpObject()
+        {
+            NestedJsonParser parser = new NestedJsonParser();
+            StringBuilder jsonBuilder = new StringBuilder();
+
+            Person personA = new Person("User", "1", null);
+            Person personB = new Person("User", "2", personA);
+
+            jsonBuilder.Append("{\"Key1\": \"1\",\"Key2\": {\"Key3\": \"1\",\"Key4\": {\"Key5\": \"4\", \"user\":user_placeholder}}}");
+            jsonBuilder.Replace("user_placeholder", parser.Serialize(personB));
+            parser.Deserialize(jsonBuilder.ToString())
+                  .Print();
+        }
+
     }
 }
